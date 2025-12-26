02635584628efb8b196f44fc2dd3f3464cd359fb
bala-amypo <bala@amypo.in>
2025-12-26 07:04:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/RateLimitEnforcementController.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.controller;
-
-import com.example.demo.entity.RateLimitEnforcement;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class RateLimitEnforcementController {
-    // Empty - just to compile
-}
